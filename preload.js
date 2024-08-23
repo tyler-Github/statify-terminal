656fc16b2799bc9bb2a6aa2ca978aa8abe5efe68
RollViral <51767087+tyler-Github@users.noreply.github.com>
2024-08-23 19:27:53 +0100

    Package support

--- preload.js
+++ preload.js
@@ -1,28 +1,36 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld("api", {
+contextBridge.exposeInMainWorld('api', {
+  // Existing methods
   executeCommandWithLogging: (command, callback) => {
-    ipcRenderer.invoke("execute-command-with-logging", command).then((result) => {
+    ipcRenderer.invoke('execute-command-with-logging', command).then((result) => {
       result.forEach(({ text, type }) => {
         callback(text, type);
       });
     });
   },
   minimizeWindow: () => {
-    ipcRenderer.send("minimize-window");
+    ipcRenderer.send('minimize-window');
   },
   closeWindow: () => {
-    ipcRenderer.send("close-window");
+    ipcRenderer.send('close-window');
   },
   toggleFullScreen: () => {
-    ipcRenderer.send("toggle-fullscreen");
+    ipcRenderer.send('toggle-fullscreen');
   },
-  readFileContent: (filename) => ipcRenderer.invoke("read-file-content", filename),
-  saveFileContent: (filename, content) => ipcRenderer.invoke("save-file-content", filename),
+  readFileContent: (filename) => ipcRenderer.invoke('read-file-content', filename),
+  saveFileContent: (filename, content) => ipcRenderer.invoke('save-file-content', filename),
   loadTerminalHistory: (callback) => {
-    ipcRenderer.once("load-terminal-history", (event, history) => {
+    ipcRenderer.once('load-terminal-history', (event, history) => {
       callback(history);
     });
   },
-  getPlatform: () => process.platform // Ensure process is not redeclared
+  getPlatform: () => process.platform, // Ensure process is not redeclared
+  
+  // New method for update status
+  onUpdateStatus: (callback) => {
+    ipcRenderer.on('update-status', (event, status) => {
+      callback(status);
+    });
+  }
 });
